e6dba08fdc6e2e904e9ff1aa34508dc98ca731c1
Aditya Sharma <adi.shar.493@gmail.com>
2023-03-12 03:36:49 +1100

    added material design v2 based frontend for story prompt input

--- RobotChildrenApp/robotchildren/views.py
+++ RobotChildrenApp/robotchildren/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def create(request):
-    return HttpResponse("Robot Children index.")
+    template = loader.get_template('robotchildren/index.html')
+    context = {"wtf":"Wtf"}
+    return HttpResponse(template.render(context, request))
 
 # Create your views here.
