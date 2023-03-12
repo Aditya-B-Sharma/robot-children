5b2dc727bd2f587e3d79c07c86b8b1ebd071925a
Marc S <salvadormarc9@gmail.com>
2023-03-12 16:17:36 +1100

    addedfirebase login

--- RobotChildrenApp/robotchildren/urls.py
+++ RobotChildrenApp/robotchildren/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from . import views
+from . import views, auth
 
 urlpatterns = [
     path('', views.create, name='create'),
-    path('textloop', views.textloop, name='textloop')
+    path('textloop', views.textloop, name='textloop'),
+    path('signIn/', auth.signIn),
+    path('postsignIn/', auth.postsignIn),
+    path('signUp/', auth.signUp, name="signup"),
+    path('logout/', auth.logout, name="log"),
+    path('postsignUp/', auth.postsignUp),
 ]
-
 
--- RobotChildrenApp/robotchildren/views.py
+++ RobotChildrenApp/robotchildren/views.py
@@ -3,9 +3,10 @@ from django.http import HttpResponse
 from django.template import loader
 
 def create(request):
-    template = loader.get_template('robotchildren/index.html')
-    context = {"wtf":"Wtf"}
-    return HttpResponse(template.render(context, request))
+    # template = loader.get_template('robotchildren/login.html')
+    # context = {"wtf":"Wtf"}
+    # return HttpResponse(template.render(context, request))
+    return render(request, "robotchildren/index.html")
 def textloop(request):
     template = loader.get_template('robotchildren/textloop.html')
     context = {"wtf":"Wtf"}
